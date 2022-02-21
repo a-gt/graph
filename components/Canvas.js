86d639e18bca13d7e9026ba68a11e9fe2945e5c3
ApexioDaCoder <apexio@hey.com>
2022-02-21 16:16:32 +0000

    tween

--- components/Canvas.js
+++ components/Canvas.js
@@ -1,85 +1,37 @@
-import { useRef, useEffect } from "react";
-
-const getPixelRatio = (context) => {
-  var backingStore =
-    context.backingStorePixelRatio ||
-    context.webkitBackingStorePixelRatio ||
-    context.mozBackingStorePixelRatio ||
-    context.msBackingStorePixelRatio ||
-    context.oBackingStorePixelRatio ||
-    context.backingStorePixelRatio ||
-    1;
-
-  return (window.devicePixelRatio || 1) / backingStore;
-};
-
-function resizeCanvasToDisplaySize(context, canvas) {
-  let ratio = getPixelRatio(context);
-  let width = getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
-  let height = getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
-
-  canvas.width = width * ratio;
-  canvas.height = height * ratio;
-  canvas.style.width = `${width}px`;
-  canvas.style.height = `${height}px`;
-}
-
-const useCanvas = (draw, options = {}) => {
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext(options.context || "2d");
-    let frameCount = 0;
-    let animationFrameId;
-    resizeCanvasToDisplaySize(context, canvas);
-    const render = () => {
-      frameCount++;
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      draw(context, frameCount);
-      animationFrameId = window.requestAnimationFrame(render);
-    };
-    render();
-    return () => {
-      window.cancelAnimationFrame(animationFrameId);
-    };
-  }, [draw]);
-  return canvasRef;
-};
+import { useRef, useEffect, useState } from "react";
+import useCanvas from "../lib/useCanvas";
+import useTween, { update } from "../lib/useTween";
 
 const Canvas = (props) => {
+  let size = 300;
+  const [stop, setStop] = useState(false);
+  const [r, start] = useTween({ radius: 0 }, { radius: size }, 500);
+  let pr = 0;
   const draw = (ctx, frameCount) => {
+    update();
     let canvas = ctx.canvas;
-    let r = (canvas.width / 2 - 10) * Math.sin(frameCount * 0.05) ** 2;
+    let x = canvas.width / 2 - size/2;
+    let y = canvas.height / 2 - size/2;
     ctx.fillStyle = "rgba(10,10,10,0.5)";
     ctx.beginPath();
-    ctx.arc(
-      canvas.width / 2 - 10,
-      canvas.height / 2 + r / 15,
-      r,
-      0,
-      2 * Math.PI
-    );
-    ctx.fill();
-    ctx.fillStyle = "rgb(255, 246, 230)";
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2 - 10, canvas.height / 2 - 10, r, 0, 2 * Math.PI);
+    ctx.arc(x, y + r.radius / 15, r.radius, 0, 2 * Math.PI);
     ctx.fill();
     ctx.fillStyle = "rgb(39, 35, 37)";
+    ctx.strokeStyle = "rgb(255, 246, 230)";
     ctx.beginPath();
-    ctx.arc(
-      canvas.width / 2 - 10 + Math.max(r - 10, 0) / 100,
-      canvas.height / 2 - 10 + Math.max(r - 10, 0) / 100,
-      Math.max(r - 10, 0),
-      0,
-      2 * Math.PI
-    );
+    ctx.arc(x, y - 10, r.radius, 0, 2 * Math.PI);
     ctx.fill();
+    ctx.lineWidth = 10;
+    ctx.stroke();
   };
 
   const canvasRef = useCanvas(draw);
 
-  return <canvas ref={canvasRef} width={100} height={100} {...props} />;
+  useEffect(() => {
+    start();
+  }, []);
+
+  return <canvas ref={canvasRef} width={500} height={500} {...props} />;
 };
 
 export default Canvas;
--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,7 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Fraunces:ital,wght@0,200;1,400&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head></Head>
       <Component {...pageProps} />
     </>
   );
